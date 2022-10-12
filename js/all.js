b8436297ad24dfceed576a1036bb348d52b99e86
JasonLi <jjason41004tw@gmail.com>
2022-10-12 22:38:42 +0800

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,14 @@
+// var length = new Vue({
+//     el : '#length_transfer',
+//     data:{
+//         l : 1
+//     }
+// })
+
+
+let vm = new Vue({
+    el:'#app',
+    data:{
+        message: 0,
+    }
+})
